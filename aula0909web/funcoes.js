ac4a92aae01a1bda124201f9a0448a9c24581122
kelly201207 <kellydasilva956@gmail.com>
2025-09-09 16:57:26 -0300

    Add files via upload

---
+++ aula0909web/funcoes.js
@@ -0,0 +1,10 @@
+"crie um programa  e não uma função que solicite dois numeros ao usuario e informe soma"
+
+// Solicita dois números ao usuário
+let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+let numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+// Calcula a soma dos dois números
+let soma = numero1 + numero2;
+// Exibe o resultado da soma
+alert("A soma dos dois números é: " + soma);
